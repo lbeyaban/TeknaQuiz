60f418e06d13e685504a436ed84b49781c52f60a
Ömer Aydin <63254747+lbeyaban@users.noreply.github.com>
2023-09-01 23:43:54 +0300

    alertler

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport')
-
 const controller = require('../controller')
 
 function checkNotAuthenticated(req, res, next) {
